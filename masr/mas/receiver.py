8f827375e7ef52c82492097b47a7c6174f77a876
WANGXuhong <wangxuhongcn@163.com>
2024-05-28 20:35:10 +0800

    f m t

--- masr/mas/receiver.py
+++ masr/mas/receiver.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 from mas import pipeline
 from sender import sending_from_mas_to_env
 from masr.typing.env import Env2MAS
@@ -9,13 +8,8 @@ ENV_SERVER_URL = "http://env_server:8002"
 
 @app.post("/receive_from_env/")
 async def receiving(message: Env2MAS):
-    
-    # Trigger the main pipeline to process the message
     processed_message = await pipeline(message)
-    
-    # Send the processed message
     await sending_from_mas_to_env(ENV_SERVER_URL, processed_message)
-    
     return {"status": "Message processed and sent"}
 
 if __name__ == "__main__":
