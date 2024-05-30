fcc6fac60ed4c4050ef35ef3f32d1e0b549f4b38
WanChengXue <chenliang1@shangahitech.edu.cn>
2024-05-30 19:15:04 +0800

    复用dev的实例

--- tests/test_env/test_env_runner.py
+++ tests/test_env/test_env_runner.py
@@ -1,19 +1,16 @@
 from masr.env_server.runner import pipeline
 from masr.models.interface import pack_mas_to_env_msg, Report
 from masr.models.task import TaskHistory, TaskItem, TaskStatus
-from masr.models.graph import GML
-from tests.test_base import task_id, report, source_code
+from tests.test_base import task_id, report, source_code, gml, task_desc
 
 from unittest.mock import patch
 from unittest import mock
 
-task = TaskItem(
-    name="test task", description="test task description", status=TaskStatus.COMPLETED
-)
+task = task_desc
 
 history = TaskHistory(history=[task])
 
-graph = GML(content="test graph")
+graph = gml
 
 pipeline_input = {
     "task_id": task_id,
