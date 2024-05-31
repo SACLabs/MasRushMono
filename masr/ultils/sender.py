14062847007608234b0e46d9f91d7f66a7219415
banana <sys555@outlook.com>
2024-05-31 11:48:11 +0800

    gen sender 改为队列获取

--- masr/ultils/sender.py
+++ masr/ultils/sender.py
@@ -1,10 +1,22 @@
 import requests
+import redis
+import time
 
 def gen_sender(dtypes, target):
+    redis_client = redis.from_url(
+        # TODO:REDIS_URL,
+        decode_responses = True,
+        encoding = "utf-8"
+    )
+    
     def sender(data):
-        if dtypes == 'MAS':
-            headers = {'Content-Type': 'application/json'}
-            return requests.post(target, json=data, headers = headers)
-        else:
-            ...
+        while True:
+            # 根据 dtypes 从对应队列获取消息
+            message = redis_client.brpop(f"{dtypes}_send_queue", 0)
+            if message:
+                try:
+                    resposne = requests.post(target, json=message)
+                except requests.RequestException as e:
+                    print(f"{dtypes}, {target}, sender error: {str(e)}")
+            time.sleep(1)
     return sender
